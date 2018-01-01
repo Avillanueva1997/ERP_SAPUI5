c8c7ce7d6d239284693efc42655fc14758ec185a
Freddy Farro <freddyfarro@gmail.com>
2018-01-01 18:20:40 -0500

    Ultimos cambios

--- controller/LOGISTICA/Mm/Mm01.controller.js
+++ controller/LOGISTICA/Mm/Mm01.controller.js
@@ -1,23 +1,88 @@
+var ConexionGlobal = sessionStorage.ConexionGlobal;
+
 sap.ui.define([
-   "sap/ui/su01/controller/BaseController"
-], function (BaseController) {
+ "sap/ui/su01/controller/BaseController",
+ "sap/ui/model/json/JSONModel",
+ "sap/m/MessageToast",
+ "sap/m/Dialog",
+ "sap/m/Button",
+ "sap/m/Text",
+ "sap/m/Label",
+ "sap/m/Input",
+ 'sap/ui/core/Fragment'
+ ], function (BaseController, JSONModel,MessageToast,Dialog,Button,Text,Label,Input,Fragment) {
    "use strict";
    return BaseController.extend("sap.ui.su01.controller.LOGISTICA.Mm.Mm01", {
 
-       onInit: function(oEvent) {
-       },
-       
-       
-       onBack: function(oEvent){
-           this.getRouter().navTo("home");
-       },
-       
-       onNew: function(oEvent){
-           this.getRouter().navTo("mmC");
-       },
-       
-       onCopy: function(oEvent){
-           this.getRouter().navTo("mmC");
-       }
-   });
-});
+     onInit: function(oEvent) {
+     },
+
+
+     onBack: function(oEvent){
+       this.getRouter().navTo("home");
+     },
+
+     onNew: function(oEvent){
+
+       var Matnr = this.byId("inpMatnr").getValue();
+       var Werks = "";
+       if (Matnr == "") {
+        this.getRouter().navTo("mmC");
+        }else{
+
+        var thes = this;
+        var cnx = JSON.parse(ConexionGlobal);
+
+        var parametros = {
+          "_Ip" : cnx[0].ip,
+          "_Usuario_servidor" : cnx[0].usuario_servidor,
+          "_Pass_servidor" : cnx[0].pass_servidor,
+          "_Base_datos" : cnx[0].base_datos,
+          "_Matnr" : Matnr,
+          "_Werks": Werks         
+        };
+
+        $.ajax({
+          data:  parametros,
+          url:   '/erp/model/ValidarMaterialSolo.php', 
+          type:  'post',
+          async: false,
+          beforeSend: function () {
+          },
+          success:  function (response) {                           
+            if (response=="") {
+              sap.m.MessageToast.show("El material ingresado no existe");
+            }else{     
+              thes.oPersonalizationDialog = new sap.ui.xmlfragment("sap.ui.su01.view.LOGISTICA.Mm.Vistas", thes);              
+              thes.getView().addDependent(thes.oPersonalizationDialog);              
+              thes.oPersonalizationDialog.open();
+            }
+          },
+          error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+          }
+        });              
+
+      }
+
+    },
+
+    onCopy: function(oEvent){
+     this.getRouter().navTo("mmC");
+   },
+
+    onSalirVistas: function(oEvent){
+      this.oPersonalizationDialog.close();
+    },
+
+    onAceptarVistas: function(oEvent){
+      var chkCompras = this.byId("chkAlmacenamiento-CbBg").getValue();  
+      var chkAlmacenamiento = this.byId("diaVistas--chkAlmacenamiento").getValue();  
+
+      alert("Compras: " + chkCompras);
+      alert("Almacenamiento: " + chkAlmacenamiento);
+
+    }
+ });
+ });
